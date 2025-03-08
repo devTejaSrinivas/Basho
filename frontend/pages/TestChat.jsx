361c24eb877a5037a549a136d7d5fc7644bc7ff9
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2025-03-09 00:15:58 +0530

    Routes Added

---
+++ frontend/pages/TestChat.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestChat = () => {
+  return <div>Test Chat Page</div>;
+};
+
+export default TestChat;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,15 @@
-import styles from "./style";
-import {
-  Billing,
-  Business,
-  CardDeal,
-  Clients,
-  CTA,
-  Footer,
-  Navbar,
-  Stats,
-  Testimonials,
-  Hero,
-} from "./components";
-
-const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
-    </div>
-
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
-);
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
+import TestChat from "../pages/TestChat";
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/testchat" element={<TestChat />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
