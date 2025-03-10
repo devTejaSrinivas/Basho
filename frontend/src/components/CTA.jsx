57004d87e80e5dfd8841989c63b2c3afe59199ca
dev_tej <112886635+devTejaSrinivas@users.noreply.github.com>
2025-03-10 09:53:43 +0530

    Get Started buttons routes fixed

--- frontend/src/components/CTA.jsx
+++ frontend/src/components/CTA.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const CTA = () => {
   const navigate = useNavigate();
   const handleButtonClick = () => {
-    navigate("/chatpage");
+    navigate("/signin");
   };
   return (
     <section
